00ee52737ed31c926d3eea72a2c4616ac436b633
Diego Patino <dipaco@gmail.com>
2021-11-22 18:24:38 -0500

    Updates to the results visualization

--- run_test.py
+++ run_test.py
@@ -4,6 +4,14 @@ import time
 from medial_axis import cpma, cpma_3d
 from matplotlib import pyplot as plt
 from skimage.morphology import medial_axis as baseline_medial_axis
+from tqdm import tqdm
+
+
+def plot_image(axis, image, title, cmap='gray', interpolation=None):
+    axis.axis('off')
+    axis.imshow(image, cmap=cmap, interpolation=interpolation)
+    axis.set_title(title, fontsize=8)
+
 
 if __name__ == '__main__':
 
@@ -18,22 +26,23 @@ if __name__ == '__main__':
 
     n_rows = 2
     n_cols = 3
-    plt.axis('off')
-    plt.tight_layout()
-    fig = plt.figure(dpi=300)
-    for im_file in all_images:
+    fig, axs = plt.subplots(2, 3, dpi=300)
+    for im_file in tqdm(all_images):
         im = imageio.imread(os.path.join(BASE_FOLDER, im_file))
 
         # Simple thresholding to create a binary mask for the shape
         mask = im > 128
 
-        # d
+        #import pdb
+        #pdb.set_trace()
+
+        # Computes the CPMA and the C-CPMA
         start = time.time()
-        ma, dist, scores_function = cpma(mask, return_scores=True)
+        ma, dist, score_function = cpma(mask, enforce_connectivity=False, return_scores=True)
         cpma_time = (time.time() - start)
 
         start = time.time()
-        connected_ma, dist = cpma(mask, enforce_connectivity=True)
+        #connected_ma, dist = cpma(mask, enforce_connectivity=True) #FIXME: Check the node statement
         c_cpma_time = (time.time() - start)
 
         # We use scikit image implementation as the baseline medial axis
@@ -41,29 +50,14 @@ if __name__ == '__main__':
         noise_medial_axis = baseline_medial_axis(mask)
         baseline_ma_time = (time.time() - start)
 
-        ax = plt.subplot(n_rows, n_cols, 1)
-        plt.imshow(mask, cmap='gray', interpolation=None)
-        ax.set_title('Mask')
+        # Plot the results
+        plot_image(axs[0, 0], mask, 'Mask')
+        plot_image(axs[0, 1], score_function, title='Score function', cmap='viridis')
+        plot_image(axs[0, 2], dist, title='Distance transform', cmap='viridis')
+        plot_image(axs[1, 0], mask.astype(int) + noise_medial_axis.astype(int), title=f'Baseline ({round(baseline_ma_time, 2)} s)', cmap='viridis')
+        plot_image(axs[1, 1], mask.astype(int) + ma.astype(int), title=f'CPMA ({round(cpma_time, 2)} s)', cmap='viridis')
+        #plot_image(ax[1, 2], mask.astype(int) + connected_ma.astype(int), title=f'C-CPMA ({round(c_cpma_time, 2)} s)', cmap='viridis')
 
-        ax = plt.subplot(n_rows, n_cols, 2)
-        plt.imshow(mask, cmap='gray', interpolation=None)
-        ax.set_title('Mask')
-
-        ax = plt.subplot(n_rows, n_cols, 3)
-        plt.imshow(scores_function, interpolation=None)
-        ax.set_title('Score function')
-
-        ax = plt.subplot(n_rows, n_cols, 4)
-        plt.imshow(mask.astype(int) + noise_medial_axis.astype(int), interpolation=None)
-        ax.set_title(f'Baseline ({round(baseline_ma_time, 2)} s)')
-
-        ax = plt.subplot(n_rows, n_cols, 5)
-        plt.imshow(mask.astype(int) + ma.astype(int), interpolation=None)
-        ax.set_title(f'CPMA ({round(cpma_time, 2)} s)')
-
-        ax = plt.subplot(n_rows, n_cols, 6)
-        plt.imshow(mask.astype(int) + connected_ma.astype(int), interpolation=None)
-        ax.set_title(f'C-CPMA ({round(c_cpma_time, 2)} s)')
-
+        #plt.tight_layout()
         plt.savefig(os.path.join(RESULTS_FOLDER, f'medial_axis_figure_{os.path.splitext(im_file)[0]}.png'))
 
